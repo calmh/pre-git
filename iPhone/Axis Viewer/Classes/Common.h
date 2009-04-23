093d09ad3510909f4cac1429599c95f342e9736e
jb <jb@2a83244e-8797-45b3-a2fb-03eebcce663c>
2009-04-23 21:41:58 +0000

    fix
    git-svn-id: svn://acro.nym.se/projekt@47 2a83244e-8797-45b3-a2fb-03eebcce663c

---
+++ iPhone/Axis Viewer/Classes/Common.h
@@ -0,0 +1,12 @@
+/*
+ *  Common.h
+ *  Axis Viewer
+ *
+ *  Created by Jakob Borg on 4/23/09.
+ *  Copyright 2009 Jakob Borg. All rights reserved.
+ *
+ */
+
+#define WEBVIEW_WIDTH 280
+#define WEBVIEW_HEIGHT 211
+#define PREVIEW_AGE 300
