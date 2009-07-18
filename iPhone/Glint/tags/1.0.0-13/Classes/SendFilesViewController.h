df8a1c853562cfc80b8ead51660b1df19322e0e3
jb <jb@2a83244e-8797-45b3-a2fb-03eebcce663c>
2009-07-18 12:04:37 +0000

    tag
    git-svn-id: svn://acro.nym.se/projekt@148 2a83244e-8797-45b3-a2fb-03eebcce663c

---
+++ iPhone/Glint/tags/1.0.0-13/Classes/SendFilesViewController.h
@@ -0,0 +1,25 @@
+//
+//  SendFilesViewController.h
+//  Glint
+//
+//  Created by Jakob Borg on 7/16/09.
+//  Copyright 2009 Jakob Borg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SKPSMTPMessage.h"
+
+@interface SendFilesViewController : UIViewController <SKPSMTPMessageDelegate> {
+        NSArray *files;
+        NSString *documentsDirectory;
+        UITableView *tableView;
+}
+
+@property (retain, nonatomic) IBOutlet UITableView *tableView;
+
+- (IBAction) switchToGPSView:(id)sender;
+- (IBAction) deleteFile:(id)sender;
+- (IBAction) sendFile:(id)sender;
+- (void) refresh;
+
+@end
