22d8bdcc9f9747899200704873b70d58ee2d06b8
jb <jb@2a83244e-8797-45b3-a2fb-03eebcce663c>
2009-07-19 12:46:31 +0000

    tag
    git-svn-id: svn://acro.nym.se/projekt@151 2a83244e-8797-45b3-a2fb-03eebcce663c

---
+++ iPhone/Glint/tags/1.0.0-14/Classes/GlintGPXWriter.h
@@ -0,0 +1,30 @@
+//
+//  GlingGPXWriter.h
+//  Glint
+//
+//  Created by Jakob Borg on 7/10/09.
+//  Copyright 2009 Jakob Borg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface GlintGPXWriter : NSObject {
+        NSString *filename;
+        bool inTrackSegment;
+        bool inFile;
+        double totalDistance;
+        int numPoints;
+}
+
+- (id)initWithFilename:(NSString*)newFilename;
+- (void)beginFile;
+- (void)endFile;
+- (void)beginTrackSegment;
+- (void)endTrackSegment;
+- (void)addPoint:(CLLocation*)loc;
+
+@property (readonly) bool inTrackSegment;
+@property (readonly) bool inFile;
+
+@end
