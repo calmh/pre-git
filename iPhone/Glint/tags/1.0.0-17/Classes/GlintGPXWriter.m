ef3cc396ef43cf5e56b96f5f6267be3c3f85a9ce
jb <jb@2a83244e-8797-45b3-a2fb-03eebcce663c>
2009-07-20 13:15:08 +0000

    tag
    git-svn-id: svn://acro.nym.se/projekt@163 2a83244e-8797-45b3-a2fb-03eebcce663c

---
+++ iPhone/Glint/tags/1.0.0-17/Classes/GlintGPXWriter.m
@@ -0,0 +1,91 @@
+//
+//  GlingGPXWriter.m
+//  Glint
+//
+//  Created by Jakob Borg on 7/10/09.
+//  Copyright 2009 Jakob Borg. All rights reserved.
+//
+
+#import "GlintGPXWriter.h"
+
+@implementation GlintGPXWriter
+
+@synthesize numPoints;
+
+- (void)dealloc
+{
+        [filename release];
+        [tracks release];
+        [last release];
+        [super dealloc];
+}
+
+- (id)initWithFilename:(NSString*)newFilename
+{
+        if (self = [self init]) {
+                filename = [NSString stringWithString:newFilename];
+                [filename retain];
+                tracks = [[NSMutableArray alloc] init];
+                minLon = minLat = maxLon = maxLat = totalDistance = 0.0;
+                numSegs = numPoints = 0;
+                last = nil;
+        }
+        return self;
+}
+
+- (void)addTrackSegment {
+        [tracks addObject:[NSMutableArray array]];
+        [last release];
+        last = nil;
+        numSegs++;
+}
+
+- (void)addTrackPoint:(CLLocation*)point {
+        if (last && [point getDistanceFrom:last] == 0.0)
+                return;
+        
+        [[tracks lastObject] addObject:point];
+        numPoints++;
+        
+        minLon = MIN(minLon, point.coordinate.longitude);
+        maxLon = MAX(maxLon, point.coordinate.latitude);
+        minLat = MIN(minLat, point.coordinate.longitude);
+        maxLat = MAX(maxLat, point.coordinate.latitude);
+
+        if (last)
+                totalDistance += [point getDistanceFrom:last];
+        [last release];
+        last = [point retain];
+}
+
+- (void)commit {
+        NSMutableString *gpxData = [NSMutableString string];
+        [gpxData appendString:@"<?xml version='1.0' encoding='ASCII' standalone='yes'?>\n"];
+        [gpxData appendString:@"<gpx version='1.1' creator='Glint http://glint.nym.se/' xmlns='http://www.topografix.com/GPX/1/1' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd'>\n"];
+        [gpxData appendFormat:@"  <!-- [numPoints]%d[/numPoints] -->\n", numPoints];
+        [gpxData appendFormat:@"  <!-- [totalDistance]%f[/totalDistance] -->\n", totalDistance];
+        [gpxData appendFormat:@"  <time>%@</time>\n", [[NSDate date] descriptionWithCalendarFormat:@"%Y-%m-%dT%H:%M:%SZ" timeZone:nil locale:[[NSUserDefaults standardUserDefaults] dictionaryRepresentation]]];
+        [gpxData appendFormat:@"  <bounds minlat='%f' minlon='%f' maxlat='%f' maxlon='%f'/>\n", minLat, minLon, maxLat, maxLon];
+        [gpxData appendString:@"  <trk>\n"];
+        for (NSArray *track in tracks) {
+                if ([track count] == 0)
+                        continue;
+                [gpxData appendString:@"    <trkseg>\n"];
+                for (CLLocation *point in track) {
+                        [gpxData appendFormat:@"      <trkpt lat='%f' lon='%f'>\n", point.coordinate.latitude, point.coordinate.longitude];
+                        [gpxData appendFormat:@"        <ele>%f</ele>\n", point.altitude];
+                        [gpxData appendFormat:@"        <time>%@</time>\n", [point.timestamp descriptionWithCalendarFormat:@"%Y-%m-%dT%H:%M:%SZ" timeZone:nil locale:[[NSUserDefaults standardUserDefaults] dictionaryRepresentation]]];
+                        [gpxData appendString:@"      </trkpt>\n"];
+                }
+                [gpxData appendString:@"    </trkseg>\n"];
+        }
+        [gpxData appendString:@"  </trk>\n"];
+        [gpxData appendString:@"</gpx>\n"];
+        [gpxData writeToFile:filename atomically:NO encoding:NSASCIIStringEncoding error:nil];
+}
+
+- (BOOL)isInTrackSegment {
+        return ([tracks count] > 0 && [[tracks lastObject] count] > 0);
+}
+
+@end
