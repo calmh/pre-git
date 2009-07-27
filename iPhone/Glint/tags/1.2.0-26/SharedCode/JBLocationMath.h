3e7ab0717fa768a382d75a4d5acf49152eb2029f
jb <jb@2a83244e-8797-45b3-a2fb-03eebcce663c>
2009-07-27 10:10:56 +0000

    tag
    git-svn-id: svn://acro.nym.se/projekt@215 2a83244e-8797-45b3-a2fb-03eebcce663c

---
+++ iPhone/Glint/tags/1.2.0-26/SharedCode/JBLocationMath.h
@@ -0,0 +1,22 @@
+//
+//  JBLocationMath.h
+//  Glint
+//
+//  Created by Jakob Borg on 7/26/09.
+//  Copyright 2009 Jakob Borg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "JBGPXReader.h"
+
+@interface JBLocationMath : NSObject {
+
+}
+
+- (float)speedFromLocation:(CLLocation*)locA toLocation:(CLLocation*)locB;
+- (float)bearingFromLocation:(CLLocation*)locA toLocation:(CLLocation*)locB;
+- (float)distanceAtPointInTime:(float)targetTime inLocations:(NSArray*)locations;
+- (float)timeAtLocationByDistance:(float)targetDistance inLocations:(NSArray*)locations;
+
+@end
