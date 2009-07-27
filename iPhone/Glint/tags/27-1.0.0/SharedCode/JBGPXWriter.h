b7310f45ec8d6376fb1b02dbd0d8d484e020ab7e
jb <jb@2a83244e-8797-45b3-a2fb-03eebcce663c>
2009-07-27 20:38:54 +0000

    tag
    git-svn-id: svn://acro.nym.se/projekt@220 2a83244e-8797-45b3-a2fb-03eebcce663c

---
+++ iPhone/Glint/tags/27-1.0.0/SharedCode/JBGPXWriter.h
@@ -0,0 +1,29 @@
+//
+//  GPXWriter.h
+//  Glint
+//
+//  Created by Jakob Borg on 7/10/09.
+//  Copyright 2009 Jakob Borg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface JBGPXWriter : NSObject {
+        NSString *filename;
+        NSMutableArray *tracks;
+        float minLon, minLat, maxLon, maxLat;
+        int numSegs, numPoints;
+        float totalDistance;
+        CLLocation *last;
+}
+
+- (id)initWithFilename:(NSString*)newFilename;
+- (void)addTrackPoint:(CLLocation*)loc;
+- (void)addTrackSegment;
+- (void)commit;
+- (BOOL)isInTrackSegment;
+
+@property (readonly, getter=numberOfTrackPoints) int numPoints;
+
+@end
---
+++ iPhone/Glint/tags/27-1.0.0/SharedCode/JBLocationMath.h
@@ -0,0 +1,36 @@
+//
+//  JBLocationMath.h
+//  Glint
+//
+//  Created by Jakob Borg on 7/26/09.
+//  Copyright 2009 Jakob Borg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "JBGPXReader.h"
+
+@interface JBLocationMath : NSObject {
+        float currentSpeed;
+        float currentCourse;
+        float totalDistance;
+        NSDate *firstMeasurement;
+        CLLocation *lastKnownPosition;
+}
+
+@property (readonly) float currentSpeed;
+@property (readonly) float averageSpeed;
+@property (readonly) float currentCourse;
+@property (readonly) float totalDistance;
+@property (retain) CLLocation *lastKnownPosition;
+
+- (void)updateLocation:(CLLocation*)location;
+- (float)speedFromLocation:(CLLocation*)locA toLocation:(CLLocation*)locB;
+- (float)bearingFromLocation:(CLLocation*)locA toLocation:(CLLocation*)locB;
+- (float)distanceAtPointInTime:(float)targetTime inLocations:(NSArray*)locations;
+- (float)timeAtLocationByDistance:(float)targetDistance inLocations:(NSArray*)locations;
+- (float)totalDistanceOverArray:(NSArray*)locations;
+- (NSArray*)startAndFinishTimesInArray:(NSArray*)locations;
+- (float)estimatedTotalDistance;
+
+@end
